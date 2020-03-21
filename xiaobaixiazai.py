5233d4a8456d2fd59f98830f5442bf0695b2bdda
fxltwdn <51752390+fxltwdn@users.noreply.github.com>
2020-03-21 22:30:36 +0800

    Add files via upload

---
+++ xiaobaixiazai.py
@@ -0,0 +1,130 @@
+"""
+def func(x):
+    if x>0:
+        print(x)
+        func(x-1)
+
+def func1(x):
+    if x>0:
+        func1(x-1)
+        print(x)
+
+def hanoi(n,a,b,c):
+    if n>0:
+        hanoi(n-1,a,c,b)
+        print("%d move from %s to %s"%(n,a,c))
+        hanoi(n-1,b,a,c)
+hanoi(3,"a","b","c")
+
+
+
+#获取基础信息数据，包括股票代码、名称、上市日期、退市日期等
+pool = pro.stock_basic(exchange='',
+                       list_status='L',
+                       adj='qfq',
+                       fields='ts_code,symbol,name,area,industry,fullname,list_date, market,exchange,is_hs')
+#print(pool.head())
+
+# 因为穷没开通创业板和科创板权限，这里只考虑主板和中心板
+pool = pool[pool['market'].isin(['主板', '中小板'])].reset_index()
+pool.to_csv(os.path.join(save_path, 'company_info.csv'), index=False, encoding='ANSI')
+
+
+for i in pool.ts_code:
+    print('正在获取第%d家，股票代码%s.' % (j, i))
+    #接口限制访问200次/分钟，加一点微小的延时防止被ban
+    path = os.path.join(save_path, 'OldData', i + '_NormalData.csv')
+    j += 1
+    # if os.path.exists(path):
+    #     continue
+    time.sleep(0.301)
+    df = pro.daily(ts_code=i,
+                   start_date=startdate,
+                   end_date=enddate,
+                   fields='ts_code, trade_date, open, high, low, close, pre_close, change, pct_chg, vol, amount')
+    df = df.sort_values('trade_date', ascending=True)
+    df.to_csv(path, index=False)
+
+
+
+"""
+import tushare as ts
+import pandas as pd
+import os
+import time
+
+"""
+获取历史数据
+"""
+
+mytoken = '32f764edd5c91565fcbfcde7d53e8d4be9bcef7c2fdf3e19b8dd2244'
+ts.set_token(mytoken)
+save_path = 'd:\stock'
+pro = ts.pro_api()
+
+
+def getNoramlData():
+    #获取基础信息数据，包括股票代码、名称、上市日期、退市日期等
+    pool = pro.stock_basic(exchange='',
+                           list_status='L',
+                           adj='qfq',
+                           fields='ts_code,symbol,name,area,industry,fullname,list_date, market,exchange,is_hs')
+    #print(pool.head())
+    print(pool)
+    
+    # 因为穷没开通创业板和科创板权限，这里只考虑主板和中心板
+    # pool = pool[pool['market'].isin(['主板', '中小板'])].reset_index()
+    # pool = pool[pool['market'].isin(['主板', '中小板', '创业板', '科创板'])].reset_index()
+    # pool = pool[pool['market'].isin(['创业板','科创板'])].reset_index()
+    pool.to_csv(os.path.join(save_path, 'company_info.csv'), index=False, encoding='ANSI')
+
+    print('获得上市股票总数：', len(pool)-1)
+    exit()
+    j = 1
+    for i in pool.ts_code:
+        print('正在获取第%d家，股票代码%s.' % (j, i))
+        #接口限制访问200次/分钟，加一点微小的延时防止被ban
+        path = os.path.join(save_path, 'OldData', i + '_NormalData.csv')
+        j += 1
+        # if os.path.exists(path):
+        #     continue
+        time.sleep(0.301)
+        df = pro.daily(ts_code=i,
+                       start_date=startdate,
+                       end_date=enddate,
+                       fields='ts_code, trade_date, open, high, low, close, pre_close, change, pct_chg, vol, amount')
+        df = df.sort_values('trade_date', ascending=True)
+        df.to_csv(path, index=False)
+
+
+def getIndexData():
+    # 上交所指数信息
+    df = pro.index_basic(market='SSE')
+    df.to_csv(os.path.join(save_path, 'SSE.csv'), index=False, encoding='ANSI')
+
+    # 深交所指数信息
+    df = pro.index_basic(market='SZSE')
+    df.to_csv(os.path.join(save_path, 'SZSE.csv'), index=False, encoding='ANSI')
+
+    # 获取指数历史信息
+    # 这里获取几个重要的指数 【上证综指，上证50，上证A指，深证成指，深证300，中小300，创业300，中小板综，创业板综】
+    index = ['000001.SH', '000016.SH', '000002.SH', '399001.SZ', '399007.SZ', '399008.SZ', '399012.SZ', '399101.SZ',
+             '399102.SZ']
+    for i in index:
+        path = os.path.join(save_path, 'OldData', i + '_NormalData.csv')
+        df = pro.index_daily(ts_code=i,
+                             start_date=startdate,
+                             end_date=enddate,
+                             fields='ts_code, trade_date, open, high, low, close, pre_close, change, pct_chg, '
+                                    'vol, amount')
+        df = df.sort_values('trade_date', ascending=True)
+        df.to_csv(path, index=False)
+
+
+if __name__ == '__main__':
+    #设置起始日期
+    startdate = '20120101'
+    enddate = '20200320'
+    #主程序
+    getNoramlData()
+    getIndexData()
